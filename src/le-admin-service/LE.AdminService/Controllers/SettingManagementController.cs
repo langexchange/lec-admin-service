523b3a5e8c333bfcf429d5c3acde0d2cae2ca7cb
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-04-28 01:58:40 +0700

    set up middleware

---
+++ src/le-admin-service/LE.AdminService/Controllers/SettingManagementController.cs
@@ -0,0 +1,19 @@
+﻿using LE.Library.Kernel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LE.AdminService.Controllers
+{
+    [Route("admin/api/settings")]
+    [ApiController]
+    public class SettingManagementController : ControllerBase
+    {
+        private readonly IRequestHeader _requestHeader;
+
+        public SettingManagementController(IRequestHeader requestHeader)
+        {
+            _requestHeader = requestHeader;
+        }
+
+
+    }
+}
--- src/le-admin-service/LE.AdminService/Startup.cs
+++ src/le-admin-service/LE.AdminService/Startup.cs
@@ -1,20 +1,15 @@
 using AutoMapper;
 using LE.AdminService.Extensions;
+using LE.AdminService.Infrastructure.Infrastructure;
 using LE.Library.Consul;
 using LE.Library.Host;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LE.AdminService
 {
@@ -60,7 +55,15 @@ namespace LE.AdminService
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseConsul();
+
+            app.UseCors(x => x
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .SetIsOriginAllowed(origin => true) // allow any origin
+              .AllowCredentials()); // allow credentials
+
+            app.UseCustomAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
